3c0725161d88428411faca7fe61625135b0519d8
Fahmiakhyar <fahmiakhyarabdillah@gmail.com>
2025-09-10 10:47:04 +0700

    create animasi input login

--- frontend/src/components/input/InputLogin.tsx
+++ frontend/src/components/input/InputLogin.tsx
@@ -1,13 +1,50 @@
+import { useState } from "react";
+
 interface InputLoginProps {
-    placeholder: string;
+  placeholder: string;
+  type: string;
+  name: string;
 }
 
-export default function InputLogin({placeholder}:InputLoginProps){
-    return(
-        
+export default function InputLogin({
+  placeholder,
+  name,
+  type,
+}: InputLoginProps) {
+  const [value, setValue] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
+  const isFloating = isFocused || value !== "";
+
+  return (
     <div className="flex items-center pt-1.5">
-      <input className="mx-auto w-[400px] h-[47px] rounded-xl border-[1px] border-[#66666666]  px-1.5 placeholder:text-sm text-sm outline-none" type="email" name="email" id=""
-      placeholder={placeholder} />
+      <div className="relative mx-auto w-[400px]">
+        {/* Input */}
+        <input
+          type={type}
+          id={name}
+          name={name}
+          placeholder=" "
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          className="peer w-[400px] h-[47px] rounded-xl border-[1px] border-[#66666666] px-3  text-sm outline-none placeholder-transparent"
+        />
+
+        {/* Label floating di antara border */}
+        <label
+          htmlFor={name}
+          className={`absolute left-3 px-1 text-gray-500 transition-all duration-200 cursor-text
+            ${
+              isFloating
+                ? "-top-2 text-xs text-[#66666666] bg-white" // label naik dan menempel di border
+                : "top-3 text-sm text-gray-400"
+            }`}
+        >
+          {placeholder}
+        </label>
+      </div>
     </div>
-    )
+  );
 }
--- frontend/src/views/auth/Login.tsx
+++ frontend/src/views/auth/Login.tsx
@@ -3,8 +3,8 @@ import InputLogin from "../../components/input/InputLogin";
 export default function Login() {
   return (
     <>
-      <InputLogin placeholder="Enter your email" />
-      <InputLogin placeholder="Enter your password" />
+      <InputLogin placeholder=" Enter your email" name="email" type="email"/>
+      <InputLogin placeholder=" Enter your password" name="password" type="password"/>
     </>
   );
 }
