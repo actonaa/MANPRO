3e991adfdc174f38c049a9eac3728b5bfb9c067f
Fahmiakhyar <fahmiakhyarabdillah@gmail.com>
2025-10-01 19:52:53 +0700

    remove component

--- frontend/src/components/button/BtnLogin.tsx
+++
@@ -1,7 +0,0 @@
-export default function BtnLogin(){
-    return(
-        <div className="flex items-center pt-2">
-        <button className="mx-auto lg:mx-0 w-[335px] md:w-[400px] h-[38px] py-[10px] px-[32px] rounded-xl bg-[#038BD1] text-sm font-bold text-white hover:bg-primary "> Masuk</button>
-        </div>
-    )
-}
--- frontend/src/views/auth/Beranda.tsx
+++ frontend/src/views/auth/Beranda.tsx
@@ -1,18 +1,30 @@
-import Navbar from "../../components/navbar/Navbar";
 import Card from "../../components/card/CardDshbrd";
 
 export default function Beranda() {
   return (
     <div className="min-h-screen bg-[#F0F0F0] bg-top bg-no-repeat  bg-[length:100%_22%]">
-      <Navbar />
-
       {/* Main Content */}
       <main className="px-8 py-4">
         {/* Logo Center */}
         <div className="flex justify-center mb-2 gap-6">
-         <Card title="Aset aktif" value={200} icon="/img/Assets.png" color="bg-accent" />
-      <Card title="Aset rusak" value={24} icon="/img/Risiko.png" color="bg-[#FFA0A0]" />
-      <Card title="Nilai aset" value="13m" icon="/img/Nilai.png" color="bg-[#8FED8E]" />
+          <Card
+            title="Aset aktif"
+            value={200}
+            icon="/img/Assets.png"
+            color="bg-accent"
+          />
+          <Card
+            title="Aset rusak"
+            value={24}
+            icon="/img/Risiko.png"
+            color="bg-[#FFA0A0]"
+          />
+          <Card
+            title="Nilai aset"
+            value="13m"
+            icon="/img/Nilai.png"
+            color="bg-[#8FED8E]"
+          />
         </div>
       </main>
     </div>
